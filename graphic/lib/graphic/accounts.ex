3acf7cf0190c248a2cdf6f4f04d40814274cb32d
JanoBourian <supermath33@icloud.com>
2024-07-25 09:52:44 -0600

    add: new account context functions

--- graphic/lib/graphic/accounts.ex
+++ graphic/lib/graphic/accounts.ex
@@ -1,6 +1,6 @@
 defmodule Graphic.Accounts do
   @moduledoc """
-  The Accounts context.
+  The Accounts context: public interface for account functionality.
   """
 
   import Ecto.Query, warn: false
@@ -9,96 +9,47 @@ defmodule Graphic.Accounts do
   alias Graphic.Accounts.User
 
   @doc """
-  Returns the list of users.
+  Returns the user with the given `id`.
 
-  ## Examples
-
-      iex> list_users()
-      [%User{}, ...]
-
+  Returns `nil` if the user does not exist.
   """
-  def list_users do
-    Repo.all(User)
+  def get_user(id) do
+    Repo.get(User, id)
   end
 
   @doc """
-  Gets a single user.
-
-  Raises `Ecto.NoResultsError` if the User does not exist.
-
-  ## Examples
-
-      iex> get_user!(123)
-      %User{}
-
-      iex> get_user!(456)
-      ** (Ecto.NoResultsError)
-
-  """
-  def get_user!(id), do: Repo.get!(User, id)
-
-  @doc """
   Creates a user.
-
-  ## Examples
-
-      iex> create_user(%{field: value})
-      {:ok, %User{}}
-
-      iex> create_user(%{field: bad_value})
-      {:error, %Ecto.Changeset{}}
-
   """
-  def create_user(attrs \\ %{}) do
+  def create_user(attrs) do
     %User{}
     |> User.changeset(attrs)
     |> Repo.insert()
   end
 
   @doc """
-  Updates a user.
+  Authenticates a user.
 
-  ## Examples
-
-      iex> update_user(user, %{field: new_value})
-      {:ok, %User{}}
-
-      iex> update_user(user, %{field: bad_value})
-      {:error, %Ecto.Changeset{}}
-
+  Returns `{:ok, user}` if a user exists with the given username
+  and the password is valid. Otherwise, `:error` is returned.
   """
-  def update_user(%User{} = user, attrs) do
-    user
-    |> User.changeset(attrs)
-    |> Repo.update()
+  def authenticate(username, password) do
+    user = Repo.get_by(User, username: username)
+
+    with %{password_hash: password_hash} <- user,
+         true <- Pbkdf2.verify_pass(password, password_hash) do
+      {:ok, user}
+    else
+      _ -> :error
+    end
   end
 
-  @doc """
-  Deletes a user.
+  # Dataloader
 
-  ## Examples
-
-      iex> delete_user(user)
-      {:ok, %User{}}
-
-      iex> delete_user(user)
-      {:error, %Ecto.Changeset{}}
-
-  """
-  def delete_user(%User{} = user) do
-    Repo.delete(user)
+  def datasource() do
+    Dataloader.Ecto.new(Repo, query: &query/2)
   end
 
-  @doc """
-  Returns an `%Ecto.Changeset{}` for tracking user changes.
-
-  ## Examples
-
-      iex> change_user(user)
-      %Ecto.Changeset{data: %User{}}
-
-  """
-  def change_user(%User{} = user, attrs \\ %{}) do
-    User.changeset(user, attrs)
+  def query(queryable, _) do
+    queryable
   end
 end
