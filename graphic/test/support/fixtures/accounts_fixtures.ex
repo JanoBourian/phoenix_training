68f8890773d882efa1d0407ee6843cdca9e1c0ab
JanoBourian <supermath33@icloud.com>
2024-07-24 22:12:24 -0600

    add: Accounts User users and its migration

---
+++ graphic/test/support/fixtures/accounts_fixtures.ex
@@ -0,0 +1,20 @@
+defmodule Graphic.AccountsFixtures do
+  @moduledoc """
+  This module defines test helpers for creating
+  entities via the `Graphic.Accounts` context.
+  """
+
+  @doc """
+  Generate a user.
+  """
+  def user_fixture(attrs \\ %{}) do
+    {:ok, user} =
+      attrs
+      |> Enum.into(%{
+        username: "some username"
+      })
+      |> Graphic.Accounts.create_user()
+
+    user
+  end
+end
