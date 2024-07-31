37fd8d58349d8b533f1f771423a34d08b71045d4
JanoBourian <supermath33@icloud.com>
2024-07-31 00:31:00 -0600

    add: mutation to signin operation

--- graphic/lib/graphic_web/resolvers/accounts.ex
+++ graphic/lib/graphic_web/resolvers/accounts.ex
@@ -2,6 +2,16 @@ defmodule GraphicWeb.Resolvers.Accounts do
   alias Graphic.Accounts
   alias GraphicWeb.Schema.ChangesetErrors
 
+  def signin(_, %{username: username, password: password}, _) do
+    case Accounts.authenticate(username, password) do
+      {:error, changeset} ->
+        {:error, message: "Invalid credentials"}
+      {:ok, user} ->
+        token = GraphicWeb.AuthToken.sign(user)
+        {:ok, %{user: user, token: token}}
+    end
+  end
+
   def signup(_, args, _) do
     case Accounts.create_user(args) do
       {:error, changeset} ->
